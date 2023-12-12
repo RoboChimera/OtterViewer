dcdff0281e79984386e1ff4ac32164c9ec5ce56c
HelloRoboRobo <gitlab@HelloRoboRobo.com>
2023-12-12 15:05:52 +0100

    Fixed Automatic Scaling and can now grab images from the internet if the file is not found

--- otterviewer.c
+++ otterviewer.c
@@ -4,6 +4,13 @@
 
 int hasScaled = 0;
 int width, height;
+int isDownloaded;
+
+void onDestroy(GtkWidget *window, gpointer data) {
+	int isDownloaded = GPOINTER_TO_INT(data);
+	printf("%i", isDownloaded);
+	gtk_main_quit();
+}
 
 void button_clicked(GtkWidget *widget, gpointer data) {
 	const gchar *entry = gtk_entry_get_text(GTK_ENTRY(data));
@@ -33,14 +40,35 @@ void button_clicked(GtkWidget *widget, gpointer data) {
 	g_print("%.2f: %ix%i\n", scale, scaledWidth, scaledHeight);
 }
 
+void scale_image(GtkWidget* widget, GtkWidget* image, GdkScreen *screen) {
+	gint screenWidth = gdk_screen_get_width(screen);
+	gint screenHeight = gdk_screen_get_height(screen);
+
+	GdkPixbuf *imgBuf = gtk_image_get_pixbuf(GTK_IMAGE(image));
+	int imageWidth = gdk_pixbuf_get_width(imgBuf);
+	int imageHeight = gdk_pixbuf_get_height(imgBuf);
+
+	while (screenWidth - 24 < imageWidth || screenHeight - 24 < imageHeight) {
+		imgBuf = gtk_image_get_pixbuf(GTK_IMAGE(image));
+		imageWidth = gdk_pixbuf_get_width(imgBuf) / 2;
+		imageHeight = gdk_pixbuf_get_height(imgBuf) / 2;
+		GdkPixbuf *scaleBuf = gdk_pixbuf_scale_simple(
+			imgBuf,
+			imageWidth, imageHeight,
+			GDK_INTERP_NEAREST
+		);
+
+		g_print("%ix%i\n", imageWidth, imageHeight);
+		gtk_image_set_from_pixbuf(GTK_IMAGE(image), scaleBuf);
+		g_object_unref(scaleBuf);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	GtkWidget *window, *grid, *image, *scale, *button;
 	gtk_init(&argc, &argv);
 
 	GdkScreen *screen = gdk_screen_get_default();
-
-	gint screenWidth = gdk_screen_get_width(screen);
-	gint screenHeight = gdk_screen_get_height(screen);
 
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(window), "OtterViewer");
@@ -48,28 +76,24 @@ int main(int argc, char *argv[]) {
 
 	grid = gtk_grid_new();
 
-	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(onDestroy), GINT_TO_POINTER(isDownloaded));
 
 	if (argv[1] != NULL) {
 		// Create the Image
-		image = gtk_image_new_from_file(argv[1]);
-		GdkPixbuf *imgBuf = gtk_image_get_pixbuf(GTK_IMAGE(image));
+		FILE *file = fopen(argv[1], "r");
+		if (file != NULL) {
+			image = gtk_image_new_from_file(argv[1]);
+			isDownloaded = 0;
+		} else {
+			int wgetBuf = snprintf(NULL, 0, "wget -O .tmp-otter %s", argv[1]) + 1;
+			char *wget = (char *)malloc(wgetBuf);
+			snprintf(wget, wgetBuf, "wget -O .tmp-otter %s", argv[1]);
 
-		int imageWidth = gdk_pixbuf_get_width(imgBuf);
-		int imageHeight = gdk_pixbuf_get_height(imgBuf);
-
-		while (screenWidth - 24 < imageWidth || screenHeight - 24 < imageHeight) {
-			imageWidth = gdk_pixbuf_get_width(imgBuf) / 2;
-			imageHeight = gdk_pixbuf_get_height(imgBuf) / 2;
-			GdkPixbuf *scaleBuf = gdk_pixbuf_scale_simple(
-				imgBuf,
-				imageWidth, imageHeight,
-				GDK_INTERP_NEAREST
-			);
-
-			g_print("%ix%i\n", imageWidth, imageHeight);
-			gtk_image_set_from_pixbuf(GTK_IMAGE(image), scaleBuf);
+			system(wget);
+			image = gtk_image_new_from_file(".tmp-otter");
+			isDownloaded = 1;
 		}
+		scale_image(window, image, screen);
 
 		// Create the input box for scaling
 		GtkWidget *scale = gtk_entry_new();
